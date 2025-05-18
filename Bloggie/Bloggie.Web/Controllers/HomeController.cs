56012a1246eabad0e33c642677a35cb1dd26013a
nafizimtiazkhannecmoney <nafiz@primotechltd.com>
2025-05-18 14:31:43 +0600

    Tag
    Sending Blog Post and Tag data by a single Model (HomeViewModel) to the view

--- Bloggie/Bloggie.Web/Controllers/HomeController.cs
+++ Bloggie/Bloggie.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Bloggie.Web.Models;
+using Bloggie.Web.Models.ViewModels;
 using Bloggie.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,17 +10,31 @@ namespace Bloggie.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IBlogPostRepository _blogPostRepository;
+        private readonly ITagRepository _tagRepository;
 
-        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository)
+        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository, ITagRepository tagRepository)
         {
             _logger = logger;
             this._blogPostRepository = blogPostRepository;
+            this._tagRepository = tagRepository;
         }
 
         public async Task<IActionResult> Index()
         {
+            // Get All Blog Post
             var blogPosts = await _blogPostRepository.GetAllAsync();
-            return View(blogPosts);
+
+            // Get All Tags
+            var tags = await _tagRepository.GetAllAsync();
+
+            // Assingning the Blog and Tag Values to the Model To send to The View
+            var Model = new HomeViewModel
+            {
+                BlogPosts = blogPosts,
+                Tags = tags
+            };
+
+            return View(Model);
         }
 
         public IActionResult Privacy()
